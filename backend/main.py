5cee2dba4b9209c7050b54e13b0423272dbcc925
Chirayu Gupta <chirayugupta032@gmail.com>
2026-03-16 14:10:52 +0530

    fix cors for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,8 @@ app = FastAPI(title="Recallio API")
 # Add CORS so your Next.js frontend can talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://recallio-sooty.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
